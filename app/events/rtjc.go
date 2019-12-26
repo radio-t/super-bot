2f84e7800dff179ef1656040a0d04de23141e9b8
Eugene <umputun@gmail.com>
2019-12-25 21:54:45 -0600

    remove gitter support, wire telegram

--- app/events/rtjc.go
+++ app/events/rtjc.go
@@ -2,24 +2,27 @@ package events
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"net"
 	"time"
 
 	log "github.com/go-pkgz/lgr"
-	"github.com/sromku/go-gitter"
 )
 
 // Rtjc for rtjc commands. Publishes whatever it got from socket
 // compatible with the legacy rtjc bot, used to push news events from news.radio-t.com
 type Rtjc struct {
-	Port   int
-	Gitter *gitter.Gitter
-	RoomID string
+	Port      int
+	Submitter Submitter
+}
+
+type Submitter interface {
+	Submit(ctx context.Context, msg string) error
 }
 
 // Listen on Port accept and forward to gitter
-func (l Rtjc) Listen() {
+func (l Rtjc) Listen(ctx context.Context) {
 	log.Printf("[INFO] rtjc listener on port %d", l.Port)
 	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", l.Port))
 	if err != nil {
@@ -34,8 +37,8 @@ func (l Rtjc) Listen() {
 			continue
 		}
 		if message, err := bufio.NewReader(conn).ReadString('\n'); err == nil {
-			if _, e := l.Gitter.SendMessage(l.RoomID, message); e != nil {
-				log.Printf("[WARN] can't send message to room %s, %v", l.RoomID, err)
+			if e := l.Submitter.Submit(ctx, message); e != nil {
+				log.Printf("[WARN] can't send message, %v", err)
 			}
 		}
 		_ = conn.Close()

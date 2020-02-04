63a6e6359bada3c11929ba33ec3bb6fe49433157
Umputun <umputun@gmail.com>
2020-02-04 13:56:23 -0600

    add link support to search bot

--- app/bot/podcasts.go
+++ app/bot/podcasts.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"regexp"
 	"strings"
 	"time"
 
@@ -26,6 +27,7 @@ type siteAPIResp struct {
 	Image      string    `json:"image,omitempty"`
 	FileName   string    `json:"file_name,omitempty"`
 	ShowNotes  string    `json:"show_notes,omitempty"`
+	Body       string    `json:"body"`
 	ShowNum    int       `json:"show_num,omitempty"`
 }
 
@@ -71,23 +73,71 @@ func (p *Podcasts) OnMessage(msg Message) (response string, answer bool) {
 }
 
 func (p *Podcasts) makeBotResponse(sr []siteAPIResp, reqText string) string {
+
+	makeRepLine := func(nl noteWithLink) string {
+		if nl.link != "" {
+			return fmt.Sprintf("- [%s](%s)", nl.text, nl.link)
+		}
+		return fmt.Sprintf("- %s", nl.text)
+	}
+
 	if len(sr) == 0 {
 		return fmt.Sprintf("ничего не нашел на запрос %q", reqText)
 	}
+
 	var res string
 	for _, s := range sr {
+		nls := p.notesWithLinks(s)
 		res += fmt.Sprintf("[Радио-Т #%d](%s) _%s_\n", s.ShowNum, s.URL, s.Date.Format("02 Jan 06"))
-		for _, s := range strings.Split(s.ShowNotes, "\n") {
-			if len(s) < 2 || strings.Contains(s, " лог чата") {
+		for _, nl := range nls {
+
+			if strings.Contains(strings.ToLower(nl.text), strings.ToLower(reqText)) {
+				res += makeRepLine(nl) + "\n"
 				continue
 			}
-			if !strings.Contains(strings.ToLower(s), strings.ToLower(reqText)) {
+
+			if strings.Contains(strings.ToLower(nl.link), strings.ToLower(reqText)) {
+				res += makeRepLine(nl) + " 🔅\n"
 				continue
 			}
-			res += fmt.Sprintf("- %s\n", s)
 		}
 		res += "\n"
 
+	}
+	return res
+}
+
+type noteWithLink struct {
+	text string
+	link string
+}
+
+// linRe is a matching regex for links from href
+var linkRe = regexp.MustCompile(`<a\s+(?:[^>]*?\s+)?href="([^"]*)"`)
+
+// notesWithLinks gets notes and matching links from body
+func (p *Podcasts) notesWithLinks(s siteAPIResp) (res []noteWithLink) {
+
+	// show notes may start with multiple \n, strip them all
+	showNotes := s.ShowNotes
+	for i := 0; i < 10; i++ {
+		showNotes = strings.TrimPrefix(showNotes, "\n")
+	}
+
+	notes := strings.Split(showNotes, "\n")
+	links := strings.Split(s.Body, "<li>")[1:] // we don't care what the 0 element was, links starts with <li>
+	for i, note := range notes {
+		if strings.Contains(note, " лог чата") || strings.Contains(note, "Темы наших слушателей") {
+			break
+		}
+		nl := noteWithLink{text: note}
+		if i < len(links) {
+			ll := linkRe.FindStringSubmatch(links[i])
+			if len(ll) >= 2 {
+				nl.link = ll[1]
+			}
+		}
+		res = append(res, nl)
 	}
 	return res
 }
--- app/bot/podcasts_test.go
+++ app/bot/podcasts_test.go
@@ -44,6 +44,36 @@ func TestPodcasts_OnMessage(t *testing.T) {
 	require.True(t, answer, "second call ok too")
 }
 
+func TestPodcasts_OnMessageWithLinks(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/search" {
+			assert.Equal(t, "limit=5&q=mongo", r.URL.RawQuery)
+			sr := []siteAPIResp{
+				{
+					URL:       "http://example.com",
+					Date:      time.Date(2020, 1, 31, 16, 45, 0, 0, time.UTC),
+					ShowNum:   123,
+					ShowNotes: "\n\n\nMongo в облаке — чем это хорошо.\nWRT54GL Linksys живее всех.\nVSCode поддался.\nMozilla строит свой Context Graph.\nВоенное искуство для борьбы с хакерами.\nВдруг — Atom плагины.\nЛунный код открыт.\nxxxx в облаке — чем это хорошо\nТемы наших слушателей\n\n\nСпонсор этого выпуска DigitalOcean\n\nаудио • лог чата\n\n",
+					Body:      "<p><img src=\"https://radio-t.com/images/radio-t/rt503.jpg\" alt=\"\" /></p>\n\n<ul>\n<li><a href=\"https://www.mongodb.com/cloud\">Mongo в облаке — чем это хорошо</a>.</li>\n<li><a href=\"http://arstechnica.com/information-technology/2016/07/the-wrt54gl-a-54mbps-router-from-2005-still-makes-millions-for-linksys/\">WRT54GL Linksys живее всех</a>.</li>\n<li><a href=\"https://code.visualstudio.com/updates\">VSCode поддался</a>.</li>\n<li><a href=\"http://venturebeat.com/2016/07/06/mozilla-is-building-context-graph-a-recommender-system-for-the-web/\">Mozilla строит свой Context Graph</a>.</li>\n<li><a href=\"http://www.businessinsider.com/cymettria-cyber-deception-2016-7\">Военное искуство для борьбы с хакерами</a>.</li>\n<li><a href=\"https://medium.com/@0x1AD2/atom-treasures-82a64ac391c\">Вдруг — Atom плагины</a>.</li>\n<li><a href=\"http://qz.com/726338/the-code-that-took-america-to-the-moon-was-just-published-to-github-and-its-like-a-1960s-time-capsule/\">Лунный код открыт</a>.</li>\n<li><a href=\"https://www.mongodb.com/cloud\">Mongo в облаке — чем это хорошо</a>.</li>\n<li>Темы наших слушателей</li>\n</ul>\n\n<p><em>Спонсор этого выпуска <a href=\"https://www.digitalocean.com\">DigitalOcean</a></em></p>\n\n<p><a href=\"https://cdn.radio-t.com/rt_podcast503.mp3\">аудио</a> • <a href=\"http://chat.radio-t.com/logs/radio-t-503.html\">лог чата</a>\n<audio src=\"https://cdn.radio-t.com/rt_podcast503.mp3\" preload=\"none\"></audio></p>\n",
+				},
+			}
+			b, err := json.Marshal(sr)
+			require.NoError(t, err)
+			w.Write(b)
+			return
+		}
+		w.WriteHeader(400)
+	}))
+	defer ts.Close()
+
+	client := http.Client{Timeout: time.Second}
+	d := NewPodcasts(&client, ts.URL, 5)
+
+	result, answer := d.OnMessage(Message{Text: "/search mongo"})
+	require.True(t, answer)
+	assert.Equal(t, "[Радио-Т #123](http://example.com) _31 Jan 20_\n- [Mongo в облаке — чем это хорошо.](https://www.mongodb.com/cloud)\n- [xxxx в облаке — чем это хорошо](https://www.mongodb.com/cloud) 🔅\n\n", result)
+}
+
 func TestPodcasts_OnMessageIgnore(t *testing.T) {
 
 	d := NewPodcasts(&http.Client{}, "http://example.com", 5)
@@ -63,4 +93,51 @@ func TestPodcasts_OnMessageFailed(t *testing.T) {
 
 	_, answer := d.OnMessage(Message{Text: "/search something"})
 	require.False(t, answer)
+}
+
+func TestPodcasts_notesWithLinks(t *testing.T) {
+	s := siteAPIResp{
+		URL:       "http://example.com",
+		Date:      time.Date(2020, 1, 31, 16, 45, 0, 0, time.UTC),
+		ShowNum:   123,
+		ShowNotes: "\n\n\nMongo в облаке — чем это хорошо.\nWRT54GL Linksys живее всех.\nVSCode поддался.\nMozilla строит свой Context Graph.\nВоенное искуство для борьбы с хакерами.\nВдруг — Atom плагины.\nЛунный код открыт.\nТемы наших слушателей\n\n\nСпонсор этого выпуска DigitalOcean\n\nаудио • лог чата\n\n",
+		Body:      "<p><img src=\"https://radio-t.com/images/radio-t/rt503.jpg\" alt=\"\" /></p>\n\n<ul>\n<li><a href=\"https://www.mongodb.com/cloud\">Mongo в облаке — чем это хорошо</a>.</li>\n<li><a href=\"http://arstechnica.com/information-technology/2016/07/the-wrt54gl-a-54mbps-router-from-2005-still-makes-millions-for-linksys/\">WRT54GL Linksys живее всех</a>.</li>\n<li><a href=\"https://code.visualstudio.com/updates\">VSCode поддался</a>.</li>\n<li><a href=\"http://venturebeat.com/2016/07/06/mozilla-is-building-context-graph-a-recommender-system-for-the-web/\">Mozilla строит свой Context Graph</a>.</li>\n<li><a href=\"http://www.businessinsider.com/cymettria-cyber-deception-2016-7\">Военное искуство для борьбы с хакерами</a>.</li>\n<li><a href=\"https://medium.com/@0x1AD2/atom-treasures-82a64ac391c\">Вдруг — Atom плагины</a>.</li>\n<li><a href=\"http://qz.com/726338/the-code-that-took-america-to-the-moon-was-just-published-to-github-and-its-like-a-1960s-time-capsule/\">Лунный код открыт</a>.</li>\n<li>Темы наших слушателей</li>\n</ul>\n\n<p><em>Спонсор этого выпуска <a href=\"https://www.digitalocean.com\">DigitalOcean</a></em></p>\n\n<p><a href=\"https://cdn.radio-t.com/rt_podcast503.mp3\">аудио</a> • <a href=\"http://chat.radio-t.com/logs/radio-t-503.html\">лог чата</a>\n<audio src=\"https://cdn.radio-t.com/rt_podcast503.mp3\" preload=\"none\"></audio></p>\n",
+	}
+
+	p := Podcasts{}
+	r := p.notesWithLinks(s)
+
+	exp := []noteWithLink{
+		{text: "Mongo в облаке — чем это хорошо.", link: "https://www.mongodb.com/cloud"},
+		{text: "WRT54GL Linksys живее всех.", link: "http://arstechnica.com/information-technology/2016/07/the-wrt54gl-a-54mbps-router-from-2005-still-makes-millions-for-linksys/"},
+		{text: "VSCode поддался.", link: "https://code.visualstudio.com/updates"},
+		{text: "Mozilla строит свой Context Graph.", link: "http://venturebeat.com/2016/07/06/mozilla-is-building-context-graph-a-recommender-system-for-the-web/"},
+		{text: "Военное искуство для борьбы с хакерами.", link: "http://www.businessinsider.com/cymettria-cyber-deception-2016-7"},
+		{text: "Вдруг — Atom плагины.", link: "https://medium.com/@0x1AD2/atom-treasures-82a64ac391c"},
+		{text: "Лунный код открыт.", link: "http://qz.com/726338/the-code-that-took-america-to-the-moon-was-just-published-to-github-and-its-like-a-1960s-time-capsule/"},
+	}
+	assert.Equal(t, exp, r)
+}
+
+func TestPodcasts_notesWithLinks2(t *testing.T) {
+	s := siteAPIResp{
+		URL:       "http://example.com",
+		Date:      time.Date(2020, 1, 31, 16, 45, 0, 0, time.UTC),
+		ShowNum:   123,
+		ShowNotes: "\n\n\nПочему квесты не помогают\nКак сделать резюме менее гадким\nSLB от Bobuk\nSLB от Umputun\nКакой длины строка еще работает\nПроблемы и решения mongo лока\nТемы наших слушателей\n\n\nаудио • radio-t.torrent • лог чата\n",
+		Body:      "<p><img src=\"https://radio-t.com/images/radio-t/rt271.jpg\" alt=\"\" /></p>\n\n<ul>\n<li>Почему <a href=\"http://37signals.com/svn/posts/3071-why-we-dont-hire-programmers-based-on-puzzles-api-quizzes-math-riddles-or-other-parlor-trick\">квесты</a> не помогают</li>\n<li>Как сделать <a href=\"http://java.dzone.com/articles/how-make-your-cv-not-suck\">резюме</a> менее гадким</li>\n<li>SLB от Bobuk</li>\n<li>SLB от Umputun</li>\n<li>Какой длины строка еще работает</li>\n<li>Проблемы и решения mongo <a href=\"http://blog.pythonisito.com/2011/12/mongodbs-write-lock.html\">лока</a></li>\n<li>Темы наших слушателей</li>\n</ul>\n\n<p><a href=\"https://cdn.radio-t.com/rt_podcast271.mp3\">аудио</a> • <a href=\"https://cdn.radio-t.com/torrents/rt_podcast271.mp3.torrent\">radio-t.torrent</a> • <a href=\"http://chat.radio-t.com/logs/radio-t-271.html\">лог чата</a><audio src=\"https://cdn.radio-t.com/rt_podcast271.mp3\" preload=\"none\"></audio></p>\n",
+	}
+
+	p := Podcasts{}
+	r := p.notesWithLinks(s)
+
+	exp := []noteWithLink{
+		{text: "Почему квесты не помогают", link: "http://37signals.com/svn/posts/3071-why-we-dont-hire-programmers-based-on-puzzles-api-quizzes-math-riddles-or-other-parlor-trick"},
+		{text: "Как сделать резюме менее гадким", link: "http://java.dzone.com/articles/how-make-your-cv-not-suck"},
+		{text: "SLB от Bobuk", link: ""},
+		{text: "SLB от Umputun", link: ""},
+		{text: "Какой длины строка еще работает", link: ""},
+		{text: "Проблемы и решения mongo лока", link: "http://blog.pythonisito.com/2011/12/mongodbs-write-lock.html"},
+	}
+	assert.Equal(t, exp, r)
 }

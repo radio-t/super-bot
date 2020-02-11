138747cc7c5d00c98fe1b668e6902f8fe062b8a6
Umputun <umputun@gmail.com>
2020-02-11 00:38:12 -0600

    fix terminator tests

--- app/events/terminator_test.go
+++ app/events/terminator_test.go
@@ -17,27 +17,27 @@ func TestTerminator_checkTerminate(t *testing.T) {
 	}
 
 	// trigger ban
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now()))
 	time.Sleep(20 * time.Millisecond)
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now()))
 	time.Sleep(20 * time.Millisecond)
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now()))
 
 	// banned
-	assert.Equal(t, ban{active: true, new: true}, term.check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: true, new: true}, term.Check(bot.User{Username: "user"}, time.Now()))
 	time.Sleep(20 * time.Millisecond)
-	assert.Equal(t, ban{active: true, new: false}, term.check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: true, new: false}, term.Check(bot.User{Username: "user"}, time.Now()))
 
 	// ban expired
 	time.Sleep(510 * time.Millisecond)
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now()))
 
 	// trigger ban again
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now()))
 	time.Sleep(20 * time.Millisecond)
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now()))
 	time.Sleep(20 * time.Millisecond)
-	assert.Equal(t, ban{active: true, new: true}, term.check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: true, new: true}, term.Check(bot.User{Username: "user"}, time.Now()))
 }
 
 func TestTerminator_checkAdmin(t *testing.T) {
@@ -49,12 +49,12 @@ func TestTerminator_checkAdmin(t *testing.T) {
 	}
 
 	// try to trigger ban
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "umputun"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "umputun"}, time.Now()))
 	time.Sleep(20 * time.Millisecond)
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "umputun"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "umputun"}, time.Now()))
 	time.Sleep(20 * time.Millisecond)
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "umputun"}, time.Now()))
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "umputun"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "umputun"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "umputun"}, time.Now()))
 }
 
 func TestTerminator_checkOk(t *testing.T) {
@@ -66,15 +66,15 @@ func TestTerminator_checkOk(t *testing.T) {
 	}
 
 	// trigger ban
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now()))
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now()))
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now()))
 	time.Sleep(51 * time.Millisecond)
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now()))
 	time.Sleep(51 * time.Millisecond)
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now()))
 	time.Sleep(51 * time.Millisecond)
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now()))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now()))
 }
 
 func TestTerminator_ignoreOldMessages(t *testing.T) {
@@ -85,13 +85,13 @@ func TestTerminator_ignoreOldMessages(t *testing.T) {
 	}
 
 	// ignore old messages
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now().Add(-12*time.Millisecond)))
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now().Add(-11*time.Millisecond)))
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now().Add(-10*time.Millisecond)))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now().Add(-12*time.Millisecond)))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now().Add(-11*time.Millisecond)))
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now().Add(-10*time.Millisecond)))
 
 	// handle messages that entered "allowed period" window
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now().Add(-9*time.Millisecond))) // penalty = 0
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now().Add(-8*time.Millisecond))) // penalty = 1
-	assert.Equal(t, ban{active: false, new: false}, term.check(bot.User{Username: "user"}, time.Now().Add(-7*time.Millisecond))) // penalty = 2
-	assert.Equal(t, ban{active: true, new: true}, term.check(bot.User{Username: "user"}, time.Now().Add(-6*time.Millisecond)))   // ban
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now().Add(-9*time.Millisecond))) // penalty = 0
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now().Add(-8*time.Millisecond))) // penalty = 1
+	assert.Equal(t, ban{active: false, new: false}, term.Check(bot.User{Username: "user"}, time.Now().Add(-7*time.Millisecond))) // penalty = 2
+	assert.Equal(t, ban{active: true, new: true}, term.Check(bot.User{Username: "user"}, time.Now().Add(-6*time.Millisecond)))   // ban
 }

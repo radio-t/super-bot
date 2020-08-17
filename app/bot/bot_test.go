2f9ad001e3bb27938fd1cbd5090ff99acb4f8e31
Umputun <umputun@gmail.com>
2020-08-17 11:32:11 -0500

    clean dup tests

--- app/bot/bot_test.go
+++ app/bot/bot_test.go
@@ -1,75 +1,61 @@
 package bot
 
 import (
+	"strings"
 	"testing"
-	"time"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
-func TestMultiBot_OnMessage(t *testing.T) {
-	bb1 := &MockInterface{}
-	bb2 := &MockInterface{}
-	bb3 := &MockInterface{}
+func TestGenHelpMsg(t *testing.T) {
+	require.Equal(t, "cmd _– description_\n", genHelpMsg([]string{"cmd"}, "description"))
+}
 
-	b := MultiBot{bb1, bb2, bb3}
+func TestMultiBotHelp(t *testing.T) {
+	b1 := &MockInterface{}
+	b1.On("Help").Return("b1 help")
+	b2 := &MockInterface{}
+	b2.On("Help").Return("b2 help")
 
-	bb1.On("OnMessage", mock.MatchedBy(func(msg Message) bool {
-		require.Equal(t, "message", msg.Text)
-		return true
-	})).Return(Response{Text: "response1", Send: true})
+	// Must return concatenated b1 and b2 without space
+	// Line formatting only in genHelpMsg()
+	require.Equal(t, "b1 help\nb2 help\n", MultiBot{b1, b2}.Help())
+}
 
-	bb2.On("OnMessage", mock.MatchedBy(func(msg Message) bool {
-		require.Equal(t, "message", msg.Text)
-		return true
-	})).Return(Response{Text: "response2", Send: true})
+func TestMultiBotReactsOnHelp(t *testing.T) {
+	b := &MockInterface{}
+	b.On("ReactOn").Return([]string{"help"})
+	b.On("Help").Return("help")
 
-	bb3.On("OnMessage", mock.MatchedBy(func(msg Message) bool {
-		require.Equal(t, "message", msg.Text)
-		return true
-	})).Return(Response{Text: "response3", Send: false})
+	mb := MultiBot{b}
+	resp := mb.OnMessage(Message{Text: "help"})
 
-	resp := b.OnMessage(Message{
-		ID:     1,
-		From:   User{Username: "name"},
-		ChatID: 123,
-		Sent:   time.Now(),
-		Text:   "message",
+	require.True(t, resp.Send)
+	require.Equal(t, "help\n", resp.Text)
+}
+
+func TestMultiBotCombinesAllBotResponses(t *testing.T) {
+	msg := Message{Text: "cmd"}
+
+	b1 := &MockInterface{}
+	b1.On("ReactOn").Return([]string{"cmd"})
+	b1.On("OnMessage", msg).Return(Response{
+		Text: "b1 resp",
+		Send: true,
+	})
+	b2 := &MockInterface{}
+	b2.On("ReactOn").Return([]string{"cmd"})
+	b2.On("OnMessage", msg).Return(Response{
+		Text: "b2 resp",
+		Send: true,
 	})
 
-	assert.Equal(t, Response{Text: "response1\nresponse2", Send: true, Pin: false, Unpin: false, Preview: false, BanInterval: 0}, resp)
+	mb := MultiBot{b1, b2}
+	resp := mb.OnMessage(msg)
 
-	bb1.AssertExpectations(t)
-	bb2.AssertExpectations(t)
-	bb3.AssertExpectations(t)
-}
-
-func TestMultiBot_ReactOn(t *testing.T) {
-	bb1 := &MockInterface{}
-	bb2 := &MockInterface{}
-	bb3 := &MockInterface{}
-
-	bb1.On("ReactOn").Return([]string{"r11", "r12"})
-	bb2.On("ReactOn").Return([]string{"r21", "r22", "r23"})
-	bb3.On("ReactOn").Return([]string{"r31"})
-
-	b := MultiBot{bb1, bb2, bb3}
-
-	assert.Equal(t, []string{"r11", "r12", "r21", "r22", "r23", "r31"}, b.ReactOn())
-}
-
-func TestMultiBot_Help(t *testing.T) {
-	bb1 := &MockInterface{}
-	bb2 := &MockInterface{}
-	bb3 := &MockInterface{}
-
-	bb1.On("Help").Return("h1")
-	bb2.On("Help").Return("h2\n")
-	bb3.On("Help").Return("h3")
-
-	b := MultiBot{bb1, bb2, bb3}
-
-	assert.Equal(t, "h1\nh2\nh3\n", b.Help())
+	require.True(t, resp.Send)
+	parts := strings.Split(resp.Text, "\n")
+	require.Len(t, parts, 2)
+	require.Contains(t, parts, "b1 resp")
+	require.Contains(t, parts, "b2 resp")
 }

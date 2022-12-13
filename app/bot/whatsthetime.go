2e0408509367ed067a70677283f09d37be822f25
Alan Kochev <alaija@users.noreply.github.com>
2022-12-14 00:15:03 +0100

    What the time? bot (#86)
    * What the time bot
    
    * Fix lint errors
    
    * Typo
    
    * Avoid to use regexp and real time in tests
    
    @alaija

---
+++ app/bot/whatsthetime.go
@@ -0,0 +1,87 @@
+package bot
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"time"
+)
+
+// WhatsTheTime answers which time is on hosts timezones
+// uses whatsthetime.data file as configuration
+type WhatsTheTime struct {
+	hosts []Host
+}
+
+// Host is structure with name and timezone
+type Host struct {
+	Name string
+	Timezone string
+}
+
+// NewWhatsTheTime makes new What's The Time bot and load data to []hosts
+func NewWhatsTheTime(dataLocation string) (*WhatsTheTime, error) {
+	log.Printf("[INFO] created WhatstTheTime bot, data location=%s", dataLocation)
+	res := WhatsTheTime{}
+	if err := res.loadTimeData(dataLocation); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+func (w *WhatsTheTime) loadTimeData(dataLocation string) error {
+	data, err := readLines(dataLocation + "/whatsthetime.data")
+	if err != nil {
+		return fmt.Errorf("can't load whatsthetime.data: %w", err)
+	}
+
+	for _, line := range data {
+		elems := strings.Split(line, "|")
+		if len(elems) != 2 {
+			log.Printf("[DEBUG] bad format %s, ignored", line)
+			continue
+		}
+		host := Host{
+			Name: elems[0],
+			Timezone: elems[1],
+		}
+		w.hosts = append(w.hosts, host)
+		log.Printf("[DEBUG] loaded basic response, %s, %s", host.Name, host.Timezone)
+	}
+	return nil
+}
+
+// OnMessage returns one entry
+func (w *WhatsTheTime) OnMessage(msg Message) (response Response) {
+	if !contains(w.ReactOn(), msg.Text) {
+		return Response{}
+	}
+
+	return Response{
+		Text: buildResponseText(time.Now(), w.hosts), 
+		Send: true,
+	}
+}
+
+func buildResponseText(now time.Time, hosts []Host) string {
+	responseString := ""
+	for _, host := range hosts {
+		location, err := time.LoadLocation(host.Timezone)
+		if err != nil {
+			log.Printf("[DEBUG] can't load location for %s: %s", host.Timezone, err)
+			continue
+		}
+		responseString += fmt.Sprintf("У %s сейчас %s\n", host.Name, now.In(location).Format("15:04"))
+	}
+	return responseString
+}
+
+// ReactOn returns reaction keys
+func (w *WhatsTheTime) ReactOn() []string {
+	return []string{"время!", "time!", "который час?"}
+}
+
+// Help returns help message
+func (w *WhatsTheTime) Help() (line string) {
+	return genHelpMsg(w.ReactOn(), "подcкажет время у ведущих")
+}
---
+++ app/bot/whatsthetime_test.go
@@ -0,0 +1,53 @@
+package bot
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWhatsTheTime_buildResponseText(t *testing.T) {
+	t.Parallel()
+
+	table := []struct {
+		in  Host
+		exp string
+	}{
+		{
+			in:  Host{Name: `Ksenks`, Timezone: `America/Los_Angeles`},
+			exp: "У Ksenks сейчас 12:20\n",
+		},
+		{
+			in:  Host{Name: "Umputun", Timezone: "America/Chicago"},
+			exp: "У Umputun сейчас 14:20\n",
+		},
+		{
+			in:  Host{Name: "Bobuk", Timezone: "Europe/Kiev"},
+			exp: "У Bobuk сейчас 23:20\n",
+		},
+		{
+			in:  Host{Name: "Gray", Timezone: "Europe/Kiev"},
+			exp: "У Gray сейчас 23:20\n",
+		},
+		{
+			in:  Host{Name: "Alek.sys", Timezone: "Europe/London"},
+			exp: "У Alek.sys сейчас 21:20\n",
+		},
+	}
+
+	mockTime := time.Date(1970, 1, 1, 20, 20, 0, 0, time.UTC)
+	for _, row := range table {
+		t.Run("", func(t *testing.T) {
+			res := buildResponseText(mockTime, []Host{row.in})
+			assert.Equal(t, row.exp, res)
+		})
+	}
+}
+
+func TestWhatsTheTime_Help(t *testing.T) {
+	b, err := NewWhatsTheTime("./../../data")
+	require.NoError(t, err)
+	require.Equal(t, "время!, time!, который час? _– подcкажет время у ведущих_\n", b.Help())
+}
